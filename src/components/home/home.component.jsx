291430da4e344de5892f059fd3cbb38e0b1fa61c
Fahrgast <felix_argast@gmx.de>
2022-09-24 13:19:44 +0200

    Home page finished

--- src/components/home/home.component.jsx
+++ src/components/home/home.component.jsx
@@ -3,24 +3,26 @@ import "./home.styles.scss";
 const Home = () => {
   return (
     <div className="home-container">
-      <h3>Hi, my name is</h3>
-      <h1>
-        Felix <span className="break"></span> Argast.
-      </h1>
-      <h2>I'm a Frontend Webdeveloper</h2>
-      <div className="text">
-        <p>
-          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Consequatur
-          magni recusandae iure excepturi quos! Modi tenetur earum amet,
-          quisquam quidem cum sequi fugit explicabo nam velit, et aliquam minus
-          placeat!
-        </p>
-        <p>
-          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Porro
-          molestiae facere, doloremque illum architecto dignissimos aliquid
-          alias mollitia nisi.
-        </p>
+      <div className="home-text-container">
+        <h3 className="hello">Hi, my name is</h3>
+        <h1 className="name">Felix Argast.</h1>
+        <h2 className="home-subheading">I'm a Frontend Developer</h2>
+        <div className="text">
+          <p>
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+            Consequatur magni recusandae iure excepturi quos! Modi tenetur earum
+            amet, quisquam quidem cum sequi fugit explicabo nam velit, et
+            aliquam minus placeat!
+          </p>
+          <p>
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Porro
+            molestiae facere, doloremque illum architecto dignissimos aliquid
+            alias mollitia nisi.
+          </p>
+        </div>
       </div>
+      <img src="" alt="" className="image-bubble" />
+      <img src="" alt="" className="me" />
     </div>
   );
 };
